e4bb4e957ac12ebfa22b12625b516c6c8880ec8c
Bourne <710150774@qq.com>
2017-07-04 21:38:40 +0800

    modify

--- js/app.js
+++ js/app.js
@@ -1,4 +1,31 @@
 /**
  *网站的入口应用程序，提供网站全局初始化和页面路由服务
  */
-var flApp = angular.module('flApp',['ui.router','CourseShowMod','CourseTakeMod','EnrollMod','ReviewMod']);
+//var flApp = angular.module('flApp',['ui.router','BaseMod','CourseShowMod','CourseTakeMod','EnrollMod','ReviewMod']);
+var flApp = angular.module('foreignApp',['ui.router','BaseMod']);
+console.log(flApp);
+/**
+ * 定义全局信息
+ */
+flApp.run(function($rootScope, $state, $stateParams){
+
+})
+/**
+ * 配置页面路由信息
+ */
+flApp.config(function($stateProvider,$urlRouterProvider){
+     $urlRouterProvider.otherwise('index');
+     $stateProvider
+        .state('index',{
+            url:'/index',
+            views:{
+                'header':{
+                    templateUrl:'tpls/header.html'
+                },
+                'navside':{
+                    templateUrl:'tpls/navside.html'
+                }
+
+            }
+        })    
+})
---
+++ js/controllers.js
@@ -0,0 +1,18 @@
+/**
+ * 基础信息模块
+ */
+var baseMod = angular.module('BaseMod',[]);
+console.log(baseMod);
+baseMod
+    .factory('',function(){
+        return;
+    })
+    .controller('HeaderCtrl',['$scope',function($scope){
+        
+    }])
+    .controller('NavCtrl',['$scope',function($scope){
+
+    }])
+/**
+ * 
+ */
